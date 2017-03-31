4991ca608f3510ea86764b188f87f918fe1c38fd
camilome6 <camilome6@gmail.com>
2017-03-31 11:24:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sistemas/subidagit/TrabajoGitHub.java
@@ -0,0 +1,14 @@
+package com.example.sistemas.subidagit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrabajoGitHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trabajo_git_hub);
+    }
+}
+
